0d975910dea5a10989cae643786334ae8bb91baa
Alwin-2984 <akcakpl2001@gmail.com>
2024-01-27 16:57:40 +0530

    header size

--- MicroBlog/src/Screens/Components/Header/Header.jsx
+++ MicroBlog/src/Screens/Components/Header/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
 
 
   return (
-    <header className="bg-gray-50 dark:dark:bg-gray-800 dark:dark:text-gray-100 h-[15vh] max-sm:h-[9vh]">
+    <header className="bg-gray-50 dark:dark:bg-gray-800 dark:dark:text-gray-100 h-[12vh] max-sm:h-[9vh]">
       <div className="mx-auto max-sm:px-0 max-sm:py-0 max-w-[90vw] max-sm:pt-5 px-6 py-8 sm:px-6 lg:px-8">
         <div className="flex items-center sm:justify-between sm:gap-4">
           <div className="relative  sm:text-5xl max-sm:pl-0 max-sm:pt-0  font-sans font-extrabold text-2xl">
@@ -29,8 +29,8 @@ export default function Header() {
             </button> */}
           </div>
         </div>
-        <hr className="mt-5"/><hr />
       </div>
+        <hr />
      
     </header>
   );
